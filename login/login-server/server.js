6efea60c53e6e58141f6d1a1d69ea29eb80339be
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-01 22:53:20 +0200

    Login - STEP - 2 - BrowserRouter, Server -first-step, Data.json, Envs, Compas

---
+++ login/login-server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+// import dotenv from "dotenv"
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h2>Welcome to our fullstack Book App!</h2>
+    <div>Our routes:<div>
+    <div>Home: <a href="/">/</a></div>
+    <div>Users: <a href="/users">/users</a></div>
+    <div>Books: <a href="/books">/books</a></div>
+    `);
+});
+
+app.get("/users", (req, res) => {
+  res.json([
+    { _id: "u1", name: "Gael", email: "gael@gmail.com", password: "gael" },
+    { _id: "u2", name: "Robert", email: "robert@gmail.com", password: "robert"},
+  ]);
+});
+
+app.get("/books", (req, res) => {
+  res.json([
+    { _id: "b1", title: "Name of the Wind", author: "Jadon Sanderson" },
+    { _id: "b2", title: "Die Verwandlung", author: "Franz Kafka" },
+    { _id: "b3", title: "Das Glasperlenspiel", author: "Hermann Hesse" },
+  ]);
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "This route does not exist" });
+});
+
+const PORT = 5000 || process.env.PORT;
+app.listen(PORT, () => {
+  console.log("Server listening at http://localhost:" + PORT);
+});
--- login/login/src/App.js
+++ login/login/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { NavLink, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-k
+      <header>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="signup">Signup</NavLink>
+          <NavLink to="login">Login</NavLink>
+          <NavLink to="dashboard">Dashboard</NavLink>
+        </nav>
+        <h2>Manage your app</h2>
+        <Routes>
+          <Route path="/" element={<div>HomePage</div>} />
+          <Route path="/signup" element={<div>SignupPage</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/dashboard" element={<div>Dashboard</div>} />
+        </Routes>
+      </header>
     </div>
   );
 }
