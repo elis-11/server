a22005a4599492327487aafe9368ceadbd4f79d9
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-03 10:59:21 +0200

    Login - STEP -5- Implemented Login Route

--- login/login-server/routes/users.router.js
+++ login/login-server/routes/users.router.js
@@ -3,19 +3,37 @@ const User = require("../models/User");
 
 const usersRouter = express.Router();
 
-
 // GET all users route
 usersRouter.get("/", async (req, res) => {
-    const usersAll= await User.find()
-      res.json( usersAll);
-    });
-    
-    // POST/users - create / signup new user
+  const usersAll = await User.find();
+  res.json(usersAll);
+});
+
+// POST/users - create / signup new user
 usersRouter.post("/", async (req, res) => {
+  const { email } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser)
+    return res
+      .status(400)
+      .json({ error: `User with email ${email} already exists` });
+
+  const userNew = await User.create(req.body);
+  res.json(userNew);
+});
+
+usersRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
   console.log(req.body);
+  const userFound = await User.findOne({
+    email,password
+  })
+  if(!userFound) {
+  return res.status(400).json({ error: "User does not exist! Try with other email / password."})
+  }
+  res.json(userFound)
+});
 
-const userNew= await User.create(req.body)
-
-res.json(userNew);
-})
 module.exports = usersRouter;
