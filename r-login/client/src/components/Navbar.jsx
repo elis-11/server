30fada3314c376ca4127daa8630d362e0027cc2a
“elis-11” <eliza.arzanukaeva@gmail.com>
2022-06-19 12:10:38 +0200

    RM -9- Logout

--- r-login/client/src/components/Navbar.jsx
+++ r-login/client/src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+
+const handleLogout=(e) => {
+e.preventDefault();
+}
+
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
       <NavLink to="signup">Signup</NavLink>
       <NavLink to="login">Login</NavLink>
-      {/* <NavLink to="logout">Logout</NavLink> */}
+      <NavLink onClick={handleLogout} to="#">Logout</NavLink>
       <NavLink to="dashboard">Dashboard</NavLink>
     </nav>
   );
